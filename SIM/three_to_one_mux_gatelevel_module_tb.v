b91a46084bc90790d319d358514e72b6b633e9d0
sg05060 <psh2018314072@gmail.com>
2022-03-24 18:25:47 +0900

    update

---
+++ SIM/three_to_one_mux_gatelevel_module_tb.v
@@ -0,0 +1,61 @@
+module three_to_one_mux_tb;
+    reg a;
+    reg b;
+    reg c;
+    reg s0;
+    reg s1;
+    wire out;
+
+    three_to_one_mux_gatelevel_module inst_0(
+        .a(a),
+        .b(b),
+        .c(c),
+        .s0(s0),
+        .s1(s1),
+        .out(out)
+    );
+
+    initial begin
+        a = 0;
+        b = 0;
+        c = 0;
+        s0 = 0;
+        s1 = 0;
+        #10
+            s1 = 0;
+            s0 = 0;
+                #10
+                    a = 1;
+                #10
+                    a = 0;
+                #10
+                    a = 1;
+        #10
+            s1 = 0;
+            s0 = 1;
+                #10
+                    b = 1;
+                #10
+                    b = 0;
+                #10 
+                    b = 1;
+        #10
+            s1 = 1;
+            s0 = 0;
+                #10
+                    c = 1;
+                #10
+                    c = 0;
+                #10
+                    c = 1;
+        #10
+            s1 = 1;
+            s0 = 1;
+                #10
+                    c = 1;
+                #10
+                    c = 0;
+                #10
+                    c = 1;
+    end
+endmodule
