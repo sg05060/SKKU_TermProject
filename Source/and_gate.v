140f4214400d4f05442eb253e3b0ed2d6085cea4
sg05060 <81350669+sg05060@users.noreply.github.com>
2022-03-24 18:30:18 +0900

    Delete Source directory

--- Source/and_gate.v
+++
@@ -1,9 +0,0 @@
-module and_gate (a, b, out); 
-	
-	input a, b;
-
-	output out;
-	
-	assign out = a & b;
-
-endmodule
--- Source/not_gate.v
+++
@@ -1,9 +0,0 @@
-module not_gate (a, out); 
-	
-	input a;
-
-	output out;
-
-	assign out = !a;
-
-endmodule
