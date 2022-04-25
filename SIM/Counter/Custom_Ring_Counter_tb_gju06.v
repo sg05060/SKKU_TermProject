88ebc0b2853342dfa5f325d3b37d8798720a98e8
gju06051 <gju06051@gmail.com>
2022-04-25 23:07:26 +0900

    modify custom_ring_counter_gju06 version testbench
    use local parameter for clock delay period and count num for other people check. If you need some modify or test, change them free.
    
    Testplan
    1) reset on and wait 15ns for reset all signal and off reset
    2) wait for posedge clock signal
    3) give count num and en signal for start
    4) waiting done signal is on
    5) if done signal is on, wait clock two times and enable signal off
    6) give double clock delay(maybe 20ns) and finish testbench

--- SIM/Counter/Custom_Ring_Counter_tb_gju06.v
+++ SIM/Counter/Custom_Ring_Counter_tb_gju06.v
@@ -1,4 +1,9 @@
 module Custom_Ring_Counter_gju06_tb;
+    
+    localparam CLOCK_DELAY  = 10;
+    localparam TEST_CNT_VAL = 30;
+    
+    
     reg             clk;
     reg             rst;
     reg             en;
@@ -6,29 +11,7 @@ module Custom_Ring_Counter_gju06_tb;
     wire    [7:0]   cnt_o;
     wire            done_o;
     
-    // making clock signal
-    initial begin
-        clk = 1'b0;
-        forever
-            #5 clk = ~clk;
-    end
-    
-    // 
-    initial begin
-        #0  clk = 0; 
-            rst = 1; 
-            en  = 0; 
-            i_num_cnt = 8'b0;
-        
-        #50 rst = 0;
-        #20 i_num_cnt = 8'd30;
-        #10 en  = 1; 
-            wait (done_o==1);
-            @(posedge clk);
-            en  = 0;
-        #1000;
-    end
-    
+    // instanciate device under test
     Custom_Ring_Counter_gju06 custom_ring_couter_inst(
         .clk        (clk), 
         .rst        (rst), 
@@ -37,5 +20,36 @@ module Custom_Ring_Counter_gju06_tb;
         .cnt_o      (cnt_o),
         .done_o     (done_o)
     );
+    
+    // generating clock signal
+    initial begin
+        clk                         = 1'b0; // clock initialize
+        forever begin
+            #(CLOCK_DELAY/2) clk    = ~clk; // clock vibration
+        end
+    end
+    
+    // generating reset signal
+    initial begin
+        rst                         = 1'b1; // reset initialize(positive reset -> initialize 1 for reset all signal)
+        #15
+        rst                         = 1'b0; // release the reset
+    end
+    
+    // testbench
+    initial begin
+        wait (rst==0);      // wait for reset release
+        @(posedge clk);     // wait for clock rising
+        
+        #3  i_num_cnt   = TEST_CNT_VAL;
+            en          = 1;
+        
+        wait (done_o==1);               // wait for counter actiavation is done
+        repeat (2) @(posedge clk);      // wait for clock signal twice
+        en  = 0;                        // stop counter actiavation 
+        #(CLOCK_DELAY * 2)
+        $stop;
+    end
+    
     
 endmodule
