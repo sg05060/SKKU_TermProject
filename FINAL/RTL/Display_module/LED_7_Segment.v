a07517fd5995dfe9f51c4e5fd6d89c668808bae3
sg05060 <psh2018314072@gmail.com>
2022-05-26 13:27:12 +0900

    fixed display

--- FINAL/RTL/Display_module/LED_7_Segment.v
+++ FINAL/RTL/Display_module/LED_7_Segment.v
@@ -33,14 +33,12 @@ module Display_module(
             one_second_counter <= 0;
         else begin
             if(one_second_counter>=99999999)
-            //if(one_second_counter>=10)  
                 one_second_counter <= 0;
             else
                 one_second_counter <= one_second_counter + 1;
         end
     end 
     assign one_second_enable = (one_second_counter==99999999)?1:0;
-    //assign one_second_enable = (one_second_counter==10)?1:0;
     always @(posedge clock_100Mhz or posedge reset)
     begin
         if(reset==1) begin
@@ -69,10 +67,10 @@ module Display_module(
     begin
         if(reset==1)
             sel <= 4'b0;
-        else if(one_second_enable==1) //&& en == 1'b1)
+        else if(one_second_enable==1) 
             sel <= sel + 1;
     end
-    assign is_done_o = (sel == 4'b1000); //&& en;
+    assign is_done_o = (sel == 4'b1000); 
 
     always @(posedge clock_100Mhz or posedge reset)
     begin 
@@ -82,32 +80,31 @@ module Display_module(
             refresh_counter <= refresh_counter + 1;
     end 
     assign LED_activating_counter = refresh_counter[19:18];
-    //assign LED_activating_counter = refresh_counter[1:0];
     // anode activating signals for 4 LEDs, digit period of 2.6ms
     // decoder to generate anode signals 
     always @(*)
     begin
         case(LED_activating_counter)
         2'b00: begin
-            Anode_Activate = 4'b0111; 
+            Anode_Activate = 4'b1000; 
             // activate LED1 and Deactivate LED2, LED3, LED4
             LED_BCD = displayed_number[sel[2:0]]/1000;
             // the first digit of the 16-bit number
             end 
         2'b01: begin 
-            Anode_Activate = 4'b1011; 
+            Anode_Activate = 4'b0100; 
             // activate LED2 and Deactivate LED1, LED3, LED4
             LED_BCD = (displayed_number[sel[2:0]] % 1000)/100;
             // the second digit of the 16-bit number
             end
         2'b10: begin 
-            Anode_Activate = 4'b1101; 
+            Anode_Activate = 4'b0010; 
             // activate LED3 and Deactivate LED2, LED1, LED4 
             LED_BCD = ((displayed_number[sel[2:0]] % 1000)%100)/10;
             // the third digit of the 16-bit number
                 end
         2'b11: begin
-            Anode_Activate = 4'b1110; 
+            Anode_Activate = 4'b0001; 
             // activate LED4 and Deactivate LED2, LED3, LED1
             LED_BCD = ((displayed_number[sel[2:0]] % 1000)%100)%10;
             // the fourth digit of the 16-bit number    
@@ -117,7 +114,6 @@ module Display_module(
     // Cathode patterns of the 7-segment LED display 
     always @(*)
     begin
-        /*
         case(LED_BCD)
         4'b0000: LED_out = 7'b0111111; // "0"     
         4'b0001: LED_out = 7'b0000110; // "1" 
@@ -130,20 +126,6 @@ module Display_module(
         4'b1000: LED_out = 7'b1111111; // "8"     
         4'b1001: LED_out = 7'b1101111; // "9" 
         default: LED_out = 7'b0111111; // "0"
-        endcase
-        */
-        case(LED_BCD)
-        4'b0000: LED_out = 7'b0000001; // "0"  
-        4'b0001: LED_out = 7'b1001111; // "1" 
-        4'b0010: LED_out = 7'b0010010; // "2" 
-        4'b0011: LED_out = 7'b0000110; // "3" 
-        4'b0100: LED_out = 7'b1001100; // "4" 
-        4'b0101: LED_out = 7'b0100100; // "5" 
-        4'b0110: LED_out = 7'b0100000; // "6" 
-        4'b0111: LED_out = 7'b0001111; // "7" 
-        4'b1000: LED_out = 7'b0000000; // "8"  
-        4'b1001: LED_out = 7'b0000100; // "9" 
-        default: LED_out = 7'b0000001; // "0"
         endcase
     end
 endmodule
